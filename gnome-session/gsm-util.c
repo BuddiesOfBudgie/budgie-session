ee69a8bd230b04e5f080b5d38874b9ef055ab704
David Mohammed <fossfreedom@ubuntu.com>
2025-12-07 13:57:32 +0000

    Switch to using budgie-desktop from gnome when looking for autostart files

--- gnome-session/gsm-util.c
+++ gnome-session/gsm-util.c
@@ -248,7 +248,7 @@ gsm_util_get_standard_autostart_dirs (void)
         for (i = 0; system_data_dirs[i]; i++) {
                 g_ptr_array_add (dirs,
                                  g_build_filename (system_data_dirs[i],
-                                                   "gnome", "autostart", NULL));
+                                                   "budgie-desktop", "autostart", NULL));
         }
 
         system_config_dirs = g_get_system_config_dirs ();
